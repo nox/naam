a88a17af5b1ceef69346411fdb2366349c5f9305
Anthony Ramine <nox@nox.paris>
2020-11-08 19:17:25 +0100

    Add builtins::Nop

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,6 +1,23 @@
 use crate::{Destination, Execute, Pc, Runner};
 
 #[derive(Clone, Copy, Debug)]
+pub struct Nop;
+
+impl<'tape, Env, In> Execute<'tape, Env, In> for Nop
+where
+    In: ?Sized,
+{
+    fn execute(
+        pc: Pc<'tape, Self>,
+        _runner: Runner<'tape, Env, In>,
+        _env: &mut Env,
+        _input: &mut In,
+    ) -> Destination<'tape> {
+        Ok(pc.next())
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 pub struct Unreachable;
 
 impl<'tape, Env, In> Execute<'tape, Env, In> for Unreachable
@@ -18,11 +35,17 @@ where
 }
 
 mod should_be_derived {
-    use super::Unreachable;
+    use super::*;
 
     use crate::debug_info::{Dump, Dumper};
 
     use core::fmt;
+
+    impl<'tape> Dump<'tape> for Nop {
+        fn dump(&self, fmt: &mut fmt::Formatter, _dumper: &Dumper<'tape>) -> fmt::Result {
+            fmt::Debug::fmt(self, fmt)
+        }
+    }
 
     impl<'tape> Dump<'tape> for Unreachable {
         fn dump(&self, fmt: &mut fmt::Formatter, _dumper: &Dumper<'tape>) -> fmt::Result {
